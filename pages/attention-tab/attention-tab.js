f80dee3a61b91fdff166d9e26a595e4ed6039c44
Android & RN <nocoding@163.com>
2019-03-25 21:09:24 +0800

    add page statu

--- pages/attention-tab/attention-tab.js
+++ pages/attention-tab/attention-tab.js
@@ -11,13 +11,32 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    pagestate: 0, //0:loading 1:success 2:error
+    attentionlist: [{
+      usericon: 'https://wechart-miniprogram-res-static.oss-cn-shanghai.aliyuncs.com/shortvideo/authoricons/11405dc79ed482afcef1a8961d1aa760.jpeg',
+      name: '1'
+    }, {
+      usericon: 'https://wechart-miniprogram-res-static.oss-cn-shanghai.aliyuncs.com/shortvideo/authoricons/11405dc79ed482afcef1a8961d1aa760.jpeg',
+      name: '2'
+    }, {
+      usericon: 'https://wechart-miniprogram-res-static.oss-cn-shanghai.aliyuncs.com/shortvideo/authoricons/11405dc79ed482afcef1a8961d1aa760.jpeg',
+      name: '3'
+    }, {
+      usericon: 'https://wechart-miniprogram-res-static.oss-cn-shanghai.aliyuncs.com/shortvideo/authoricons/11405dc79ed482afcef1a8961d1aa760.jpeg',
+      name: '3'
+    }, {
+      usericon: 'https://wechart-miniprogram-res-static.oss-cn-shanghai.aliyuncs.com/shortvideo/authoricons/11405dc79ed482afcef1a8961d1aa760.jpeg',
+      name: '3'
+    }, {
+      usericon: 'https://wechart-miniprogram-res-static.oss-cn-shanghai.aliyuncs.com/shortvideo/authoricons/11405dc79ed482afcef1a8961d1aa760.jpeg',
+      name: '3'
+    }]
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    
+
   }
 })
